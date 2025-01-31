06136036bde02e2dce3d4206872b89ba90cf6ebb
Yuvaprakash24 <114161768+Yuvaprakash24@users.noreply.github.com>
2025-01-31 20:41:33 +0530

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -41,6 +41,8 @@ const Register = () => {
           </div>
 
           <button type="submit" className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">Create Account</button>
+
+          <label className="block text-sm font-medium text-gray-700">If already a user! <a href='https://digital-solutions-frontend.vercel.app/login'>LogIn</a></label>
         </form>
       </div>
     </div>
